dc8940ece12230778af91a5db2671814440f1844
Kevin Mulholland <kevin.mulholland@yahoo.com>
2017-08-12 13:52:59 -0400

    Setting up

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.master')
+
+@section('content')
+	<div class="Container">
+		hello
+	</div>
+@endsection
---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Code Template</title>
+	<link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+
+@yield('content')
+
+</body>
+</html>
