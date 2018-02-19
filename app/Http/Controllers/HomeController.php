c43c412b8fbab36bf4cff8c7c68ce6b460d714f9
kmulholland <kmulholland@awg.co>
2018-02-19 13:02:40 -0500

    Updated for new Alert Components

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        flash('Success!', 'Something has NOT gone terribly wrong!')->tertiary();
+
+        return view('home.index');
+    }
+}
--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -1,19 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<title>Code Template</title>
-	<link rel="stylesheet" href="/css/app.css">
-	<meta name="viewport" content="width=device-width initial-scale=1">
+    <meta charset="UTF-8">
+    <title>Code Template</title>
+    <link rel="stylesheet" href="/css/app.css">
+    <meta name="viewport" content="width=device-width initial-scale=1">
 </head>
 <body>
 <div id="app">
-	
-	<nav>@include('layout.nav')</nav>
 
-	<div class="Wrapper">
-		@yield('content')
-	</div>
+    <nav>@include('layout.nav')</nav>
+
+    <div class="Wrapper">
+        @yield('content')
+    </div>
+
+    <alert
+        title="{{ session('flash_message')['title'] }}"
+        message="{{ session('flash_message')['body'] }}"
+        type="{{ session('flash_message')['level'] }}"
+        important="{{ session('flash_message')['important'] }}"
+    ></alert>
 
 </div>
 <script src="/js/vendor.js"></script>
